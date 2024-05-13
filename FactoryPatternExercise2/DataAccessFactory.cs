a0630c4b0ae1a8d0f8a177e4e5c66219e4907b16
Levi Joyce <linuxgamer82@gmail.com>
2024-05-13 15:54:31 -0400

    Factory lesson 2 done

--- FactoryPatternExercise2/DataAccessFactory.cs
+++ FactoryPatternExercise2/DataAccessFactory.cs
@@ -10,32 +10,28 @@ namespace FactoryPatternExercise2
     {
         public static IDataAccess GetCards(string cardType)
         {
-            while (true)
+            switch (cardType.ToLower())
             {
-                switch (cardType.ToLower())
-                {
-                    case "pokemon":
-                    case "list":
-                        return new ListDataAccess();
+                case "pokemon":
+                case "list":
+                    return new ListDataAccess();
 
-                    case "yugioh":
-                    case "sql":
-                        return new SQLDataAcess();
+                case "yugioh":
+                case "sql":
+                    return new SQLDataAcess();
 
-                    case "magic the gathering":
-                    case "mongo":
-                        return new MongoDataAcess();
+                case "magic the gathering":
+                case "mongo":
+                    return new MongoDataAcess();
 
-                    case "Exit":
-                        Console.WriteLine("Exiting the Database");
-                        Environment.Exit(0);
-                        return null;
-
-                    default:
-                        Console.WriteLine("Please choose from the correct Data bases.");
-                        return null;
-                }
-
+               /* case "Exit":
+                    Console.WriteLine("Exiting the Database");
+                    Environment.Exit(0);
+                    return null;
+*/
+                default:
+                    Console.WriteLine("Please choose from the correct Data bases.");
+                    return null;
             }
         }
     }
--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -4,29 +4,52 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello please choose from the differnt databases to veiw our cards and prices.\n Pokemon cards are in the List Data base\nYuGiOh cards are in the SQL database\nMagic the Gathering cards are in the Mongo database.");
-            string cardType = Console.ReadLine();
-
-            IDataAccess ct = DataAccessFactory.GetCards(cardType);
-            if (ct != null)
+            while (true)
             {
-                List<Product> products = ct.LoadData();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Hello please choose from the differnt databases to veiw our cards and prices.\nPokemon cards are in the List Data base\nYuGiOh cards are in the SQL database\nMagic the Gathering cards are in the Mongo database.");
+                string cardType = Console.ReadLine();
+                Console.ResetColor();
 
-                Console.WriteLine("###################################");
+                IDataAccess ct = DataAccessFactory.GetCards(cardType);
 
-                Console.WriteLine("Here are you cards!");
-                for (int index = 0; index < products.Count; index++)
+                if (ct != null)
                 {
-                    Console.WriteLine($"Card Name: {products[index].Name} || Price: {products[index].Price,0:c}");
-                }
-                Console.WriteLine("Do you wish to save your data? Y/N");
-                string saveChoice = Console.ReadLine();
+                    List<Product> products = ct.LoadData();
 
-                if (saveChoice.ToLower() == "y")
-                {
-                    ct.SaveData(List<Product> products);
-                    Console.WriteLine("Your cards were saved to the Database.");
+                    Console.WriteLine("###################################");
+
+                    Console.WriteLine("Here are you cards!");
+                    Console.ForegroundColor= ConsoleColor.Green;
+                    for (int index = 0; index < products.Count; index++)
+                    {
+                        Console.WriteLine($"Card Name: {products[index].Name} || Price: {products[index].Price,0:c}");
+                    }
+                    Console.WriteLine("Do you wish to save your data? Y/N");
+                    string saveChoice = Console.ReadLine();
+
+                    if (saveChoice.ToLower() == "y")
+                    {
+                        ct.SaveData(products);
+                        Console.WriteLine("Your cards were saved to the Database.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Data was not saved");
+                    }
+                    Console.ResetColor ();
+                    Console.WriteLine("Would like to look at another database? Y/N");
+                      
+                    string choice = Console.ReadLine().ToLower();
+
+                    if (choice == "n")
+                    {
+                        Console.WriteLine("Exiting the Program");
+                        Environment.Exit(0);
+                    }
+                    
                 }
+
 
             }
 
